d95973fd57c639814bd3d9def3663d59cdca082d
mrouhiainen <mikael.rouhiainen@smartott.com>
2017-02-17 10:47:39 -0700

    some authentication files, still working on user features - using my github email to show activity now

---
+++ app/views/contact.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.site')
+
+@section('content')
+	<div class="content">
+		<div class="contactBox">
+			<div class="email">contact@mikaelr.ca</div>
+		</div>
+	</div>
+@endsection
---
+++ app/views/portfolio.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.site')
+
+@section('content')
+            <div class="content">
+                <div class="title">Mikael Rouhiainen, EIT</div>
+            </div>
+@endsection
