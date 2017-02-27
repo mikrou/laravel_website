57cd10e076cb1826df495850050a0955a593c7ae
Mikael Rouhiainen <mrouhiai@gmail.com>
2017-02-26 21:56:02 -0700

    removed auth requirement for blog, adding some things to portfolio

--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
             <div class="content">
-                <div class="title">Mikael Rouhiainen, EIT</div>
+            My Github account: <a target="_blank"href="https://github.com/mikrou">mikrou</a>
+                <ul>
+                	<li><a href="game">Cannon Climb</a></li>
+                	<li>Rock Paper Scissors Lizard Spock, Javascript style</li>
+                	<li><a href="/">My artwork</a></li>
+                	<li><a href="https://wildtv.ca/html5">WildTV app</a></li>
+                	<li><a target="_blank" href="http://myhuntfix.com">MyHuntFix</a></li>
+
+                </ul>
             </div>
 @endsection
