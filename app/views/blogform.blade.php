42d159a9a16cfb18e360724cc00a524797e6da7c
mrouhiainen <mikael.rouhiainen@smartott.com>
2017-03-07 15:20:18 -0700

    improved the blogform design

--- app/views/blogform.blade.php
+++ app/views/blogform.blade.php
@@ -5,17 +5,24 @@
             	<div class="col-md-12">
                     <h1 class="pageTitle" style="text-align:left;">Create a blog post:</h1>
                     <div class="panel panel-default">
-                	<form class="blogpost form-vertical" role="form" method="POST" action="{{ url('/blog/post') }}">
+                	<form class="blogpost form-horizontal" role="form" method="POST" action="{{ url('/blog/post') }}">
                      {{ csrf_field() }}
-                		<label for="title">Title:</label>
-                            <input type="text" name="title">
-                        <label for="small_desc">Small description:</label>
-                        <input type="text" name="small_desc">
-                        <label for="body">Blog body:</label>
-                        <input type="text" name="body">
-                        <button type="submit">Create blog post</button>
-                        <label for="imageurl">Image url (optional)</label>
-                        <input type="text" name="imageurl">
+                     <div class="form-group">
+                		<label for="title" class=" col-md-2 control-label">Title:</label>
+                        <input type="text" name="title" class="col-md-4">
+                    </div>
+                    <div class="form-group">
+                        <label for="small_desc" class="col-md-2 control-label">Small description:</label>
+                        <input type="text" name="small_desc" class="col-md-8">
+                    </div>
+                    <div class="form-group">
+                        <label for="body" class="col-md-2 control-label">Blog body:</label>
+                        <textarea type="text" name="body" class="col-md-9" rows="18"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="imageurl" class="col-md-2 control-label">Image url (optional):</label>
+                        <input type="text" name="imageurl" class="col-md-8">
+                    </div>
                         <button type="submit">Create blog post</button>
                 	</form>
                     </div>
