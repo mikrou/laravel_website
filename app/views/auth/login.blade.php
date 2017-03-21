0d5efa94add550babe5eac11a8a096cccdbce87b
mrouhiainen <mikael.rouhiainen@smartott.com>
2017-03-21 10:12:57 -0600

    code cleanup

--- app/views/auth/login.blade.php
+++ app/views/auth/login.blade.php
@@ -2,68 +2,63 @@
 
 @section('content')
 <div class="row">
-<div class="loginBox col-md-8 col-md-offset-2">
-	<div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal loginForm" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
-
+    <div class="loginBox col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">Login</div>
+            <div class="panel-body">
+                <form class="form-horizontal loginForm" role="form" method="POST" action="{{ url('/login') }}">
+                    {{ csrf_field() }}
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                        <div class="col-md-6">
+                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
                                 @endif
-                            </div>
                         </div>
+                    </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password">
-
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <label for="password" class="col-md-4 control-label">Password</label>
+                        <div class="col-md-6">
+                            <input id="password" type="password" class="form-control" name="password">
                                 @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
                                 @endif
-                            </div>
                         </div>
+                    </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-3">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-3">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="remember"> Remember Me
+                                </label>
                             </div>
                         </div>
+                    </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-3">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i> Login
-                                </button>
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-3">
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fa fa-btn fa-sign-in"></i> Login
+                            </button>
 
-                                <a class="btn btn-link forgotPassword" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
-                            </div>
+                            <a class="btn btn-link forgotPassword" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
                         </div>
-                        <div class="form-group">
-                            <div class="col-md-12">
-                                <a class="btn btn-info" href="{{ url('/register') }}">Create a Free Account</a>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-md-12">
+                            <a class="btn btn-info" href="{{ url('/register') }}">Create a Free Account</a>
                             </div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
-</div>
+        </div>
+    </div>
 </div>
 @endsection
--- app/views/blogform.blade.php
+++ app/views/blogform.blade.php
@@ -1,31 +1,31 @@
 @extends('layouts.site')
 
 @section('content')
-            <div class="row">
-            	<div class="col-md-12">
-                    <h1 class="pageTitle" style="text-align:left;">Create a blog post:</h1>
-                    <div class="panel panel-default">
-                	<form class="blogpost form-horizontal" role="form" method="POST" action="{{ url('/blog/post') }}">
-                     {{ csrf_field() }}
-                     <div class="form-group">
-                		<label for="title" class=" col-md-2 control-label">Title:</label>
-                        <input type="text" name="title" class="col-md-4">
-                    </div>
-                    <div class="form-group">
-                        <label for="small_desc" class="col-md-2 control-label">Small description:</label>
-                        <input type="text" name="small_desc" class="col-md-8">
-                    </div>
-                    <div class="form-group">
-                        <label for="body" class="col-md-2 control-label">Blog body:</label>
-                        <textarea type="text" name="body" class="col-md-9" rows="18"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="imageurl" class="col-md-2 control-label">Image url (optional):</label>
-                        <input type="text" name="imageurl" class="col-md-8">
-                    </div>
-                        <button type="submit">Create blog post</button>
-                	</form>
-                    </div>
+<div class="row">
+   	<div class="col-md-12">
+        <h1 class="pageTitle" style="text-align:left;">Create a blog post:</h1>
+        <div class="panel panel-default">
+            <form class="blogpost form-horizontal" role="form" method="POST" action="{{ url('/blog/post') }}">
+            {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="title" class=" col-md-2 control-label">Title:</label>
+                    <input type="text" name="title" class="col-md-4">
                 </div>
-            </div>
+                <div class="form-group">
+                    <label for="small_desc" class="col-md-2 control-label">Small description:</label>
+                    <input type="text" name="small_desc" class="col-md-8">
+                </div>
+                <div class="form-group">
+                    <label for="body" class="col-md-2 control-label">Blog body:</label>
+                    <textarea type="text" name="body" class="col-md-9" rows="18"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="imageurl" class="col-md-2 control-label">Image url (optional):</label>
+                    <input type="text" name="imageurl" class="col-md-8">
+                </div>
+                <button type="submit">Create blog post</button>
+            </form>
+        </div>
+    </div>
+</div>
 @endsection
